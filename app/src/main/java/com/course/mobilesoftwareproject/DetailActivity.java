18f9dd54d698263d9111d51363e528e85b515bec
minwoo0419 <ghdl464@naver.com>
2023-12-02 15:35:40 +0900

    feat : 삭제하기 추가

--- app/src/main/java/com/course/mobilesoftwareproject/DetailActivity.java
+++ app/src/main/java/com/course/mobilesoftwareproject/DetailActivity.java
@@ -2,10 +2,15 @@ package com.course.mobilesoftwareproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.course.mobilesoftwareproject.structure.FoodDetail;
 import com.course.mobilesoftwareproject.structure.MealDetail;
 
@@ -18,10 +23,12 @@ public class DetailActivity extends AppCompatActivity {
     List<FoodDetail> foodDetailList;
     TextView typeView, dateView, whereView, priceView, reviewView;
     ImageView imageView;
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dbHelper = new DBHelper(this);
         mealDetailRepository = new MealDetailRepository(this);
         mealDetail = mealDetailRepository.getMealDetail(getIntent().getLongExtra("id", -1L));
         foodDetailList = mealDetail.getFoods();
@@ -56,5 +63,21 @@ public class DetailActivity extends AppCompatActivity {
     public static String formatNumberWithCommas(long number) {
         NumberFormat numberFormat = NumberFormat.getInstance();
         return numberFormat.format(number) + "원";
+    }
+    public void delete(View view){
+        long id = getIntent().getLongExtra("id", -1L);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        try{
+            db.delete("foodlist", "id=?", new String[]{String.valueOf(id)});
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            db.close();
+            Toast.makeText(this, "삭제가 완료되었습니다.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
